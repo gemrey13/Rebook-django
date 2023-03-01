278adc9eaab6c10bcaccb2e54d52056e8d6dbf1e
gemrey <gemreyranola@gmail.com>
2023-03-01 08:26:19 +0800

    Add llogin modal and singup modal

--- backend/feed/static/js/home.js
+++ backend/feed/static/js/home.js
@@ -13,3 +13,57 @@ for (i = 0; i < coll.length; i++) {
     }
   });
 }
+
+
+
+// Get the modal
+var loginModal = document.getElementById("loginModal");
+var signupModal = document.getElementById("signUpModal");
+
+
+// Get the button that opens the modal
+var loginBtn = document.getElementById("loginBtn");
+var signUpBtn = document.getElementById("signUpBtn");
+
+
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+var spanLogin = document.getElementsByClassName("closeLogin")[0];
+
+
+
+
+// When the user clicks the button, open the modal 
+loginBtn.onclick = function() {
+  loginModal.style.display = "block";
+}
+signUpBtn.onclick = function() {
+  signupModal.style.display = "block";
+}
+
+
+
+
+// When the user clicks on <span> (x), close the modal
+spanLogin.onclick = function() {
+  loginModal.style.display = "none";
+}
+span.onclick = function() {
+  signupModal.style.display = "none";
+}
+
+
+
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    loginModal.style.display = "none";
+  }
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    signupModal.style.display = "none";
+  }
+}
--- backend/feed/views.py
+++ backend/feed/views.py
@@ -1,21 +1,20 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse
 
 from .models import *
 
 
 
 
-
-
 def home(request):
-	posts = Post.objects.all()[:2]
+	posts = Post.objects.all()
 
 	if request.method == "POST":
 		title = request.POST['title']
 		content = request.POST['content']
 		image = request.FILES['image']
 		post = Post(title=title, content=content, image=image)
+		
 		post.save()
 		return redirect('home')
 
